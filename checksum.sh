58a9fa91017771c9d472c837e28d90d1d38237ff
socket772 <socket772@protonmail.com>
2025-06-12 10:00:43 +0200

    checksum uses a tempfile. Added menu to copiautils

--- checksum.sh
+++ checksum.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/bash
 set -eu
 
-printf "
-1) genera il checksum
+printf "1) genera il checksum
 2) controlla il checksum
 0) Annulla
 "
 read -n1 -r -s scelta
 case "$scelta" in
-    "1") find -L "$1" -type f -exec md5sum {} \; > checksum.md5;;
-    "2") md5sum -c "$1";;
+    "1") cd "$1" && find -L ./ -type f -exec md5sum {} \; > checksum.md5.temp && mv checksum.md5.temp checksum.md5;;
+    "2") cd "$1" && md5sum --quiet -c checksum.md5;;
     *) echo "Operazione annullata";;
 esac
--- creabackup.sh
+++ creabackup.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/bash
 set -eu
 
-# RSYNC
-rsync -aLvt --progress "$1" "$2/$(date +'%Y-%m-%d_%H-%M-%S')/"
+conferma () {
+    printf "ne sei sicuro (y/N)
+    "
+    read -n1 -r -s isok
+    case "$isok" in
+    "y") return 0;;
+    *) return 1;;
+esac
+
+}
+
+printf "1) Copia in cartella formattata
+2) Copia cartella senza rinomina
+3) Copia cartella senza rinomina, CANCELLA DIFFERENZE
+0) Annulla
+"
+read -n1 -r -s scelta
+case "$scelta" in
+    "1") cd "$1" && rsync -aviP "./" "$2/$(date +'%Y-%m-%d_%H-%M-%S')/";;
+    "2") rsync -aviP "$1" "$2";;
+    "3") conferma && rsync -aviP --delete "$1" "$2";;
+    *) echo "Operazione annullata";;
+esac
